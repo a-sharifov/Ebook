83ef2f95b78b51397f37a50f56da598be2ca00f7
Akber <akber.sharifov2004@gmail.com>
2024-04-20 23:49:27 +0400

    More update.

---
+++ crs/Services/Book/Application/Books/Commands/AddBook/AddBookCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Books.Commands.AddBook;
+
+internal sealed class AddBookCommandValidator : AbstractValidator<AddBookCommand>
+{
+    public AddBookCommandValidator()
+    {
+        RuleFor(x => x.Price);
+    }
+}
--- crs/Services/Book/Persistence/Migrations/20240420194418_Initialize_Database.Designer.cs
+++ crs/Services/Book/Persistence/Migrations/20240420194418_Initialize_Database.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence.DbContexts;
@@ -11,9 +12,11 @@ using Persistence.DbContexts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(BookDbContext))]
-    partial class BookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240420194418_Initialize_Database")]
+    partial class Initialize_Database
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
